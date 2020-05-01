c0b110208dee19b41d2e94f64cb35dac61d601fd
ALESIQ <alesiq@gmail.com>
2020-04-30 22:53:06 -0300

    Configurando Controllers

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, request, redirect, render_template
 from config import app_config, app_active
 from flask_sqlalchemy import SQLAlchemy
+from controller.User import UserController
 
 
 config = app_config[app_active]
@@ -21,5 +22,44 @@ def create_app(config_name):
     @app.route('/')
     def index():
         return 'Hello Word!'
+
+    @app.route('/login/')
+    def login():
+        return 'Aqui entrará a tela de login'
+
+    @app.route('/login/', methods=['POST'])
+    def login_post():
+        user = UserController()
+        email = request.form['email']
+        password = request.form['password']
+
+        result = user.login(email, password)
+
+        if result:
+            return redirect('/admin')
+        else:
+            return render_template('login.html',
+                                   data={'status': 40,
+                                         'msg': 'Dados de usuário incorretos',
+                                         'type': None})
+
+    @app.route('/recovery-password/')
+    def recovery_password():
+        return 'Aqui entrará a tela de recuperar senha'
+
+    @app.route('/recovery-password/', methods=['POST'])
+    def send_recovery_password():
+        user = UserController()
+
+        result = user.recovery(request.form['email'])
+
+        if result:
+            msg = 'E-mail de recuperação enviado com sucesso'
+            return render_template('recovery.html', data={'status': 200,
+                                                          'msg': msg})
+        else:
+            msg = 'Erro ao enviar e-mail de recuperação'
+            return render_template('recovery.html', data={'status': 401,
+                                                          'msg': msg})
 
     return app
